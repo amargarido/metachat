03537007bec88faad7a5b948fcab5dc38a9feedd
Alberto Margarido <alberto@meta.eti.br>
2022-06-14 14:56:51 -0300

    Rust server updated to 2021 edition

--- rustServer/src/main.rs
+++ rustServer/src/main.rs
@@ -1,25 +1,20 @@
-#![deny(warnings)]
-extern crate pretty_env_logger;
-extern crate warp;
-
-use warp::{Filter, Future, Stream};
-
-fn main() {
-    pretty_env_logger::init();
-
+use futures::StreamExt;
+use futures::FutureExt;
+use warp::Filter;#[tokio::main]
+async fn main() {
     let routes = warp::path("echo")
-        // The `ws2()` filter will prepare the Websocket handshake.
-        .and(warp::ws2())
-        .map(|ws: warp::ws::Ws2| {
-            // And then our closure will be called when it completes...
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
             ws.on_upgrade(|websocket| {
-                // Just echo all messages back...
                 let (tx, rx) = websocket.split();
-                rx.forward(tx).map(|_| ()).map_err(|e| {
-                    eprintln!("websocket error: {:?}", e);
+                rx.forward(tx).map(|result| {
+                    if let Err(e) = result {
+                        eprintln!("websocket error: {:?}", e);
+                    }
                 })
             })
         });
+    
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030));
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
